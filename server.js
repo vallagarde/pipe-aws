6c9af2c499aafc77302630d17d90eb31c2bc471d
Vallagarde <val.lagarde29@gmail.com>
2022-06-03 11:14:18 +0200

    API V2

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const server = app.listen(
   )
 );
 
-app.get("/", (req, res) => res.send("API Running"));
+app.get("/", (req, res) => res.send("API Running V2"));
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
